23cf7e7187d9e7c429ccbab59359986ae9fdb39a
yurirod <yurirobins@gmail.com>
2021-11-22 15:28:22 -0500

    Added PageNotFound.js

---
+++ ReactJs/ReactJs.Web/ClientApp/src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class NotFoundPage extends Component {
+    static displayName = NotFoundPage.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Page Not Found!</h1>
+            </div>
+        );
+    }
+}
